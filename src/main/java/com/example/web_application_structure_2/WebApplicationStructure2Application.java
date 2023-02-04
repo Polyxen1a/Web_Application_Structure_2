8a378db38e0574dda1278594c7787532eeffbbda
Polyxen1a <maslowa@inbox.ru>
2023-02-04 20:48:24 +0300

    Initial commit

---
+++ src/main/java/com/example/web_application_structure_2/WebApplicationStructure2Application.java
@@ -0,0 +1,13 @@
+package com.example.web_application_structure_2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebApplicationStructure2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebApplicationStructure2Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/web_application_structure_2/WebApplicationStructure2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.web_application_structure_2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebApplicationStructure2ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
